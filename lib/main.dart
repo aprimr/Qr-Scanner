4a198e96408e7bab673ff9434278236eb3b8fb7b
aprimr <aprimregmi24@gmail.com>
2026-01-26 12:01:10 +0545

    feature/add: routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qr_code/screens/scan.dart';
+import 'package:qr_code/utils/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final String title = "QR Scanner";
-
-    return MaterialApp(title: title);
+    return MaterialApp(
+      title: title,
+      debugShowCheckedModeBanner: false,
+      routes: {AppRoutes.homeRoute: (context) => Scan()},
+    );
   }
 }
